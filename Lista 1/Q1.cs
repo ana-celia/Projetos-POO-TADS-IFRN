e840331de54c699c6ffd0c68096d6f197e828ad6
ana-celia <112821127+ana-celia@users.noreply.github.com>
2022-10-10 10:09:25 -0300

    Update Q1.cs
    comentário removido

--- Lista 1/Q1.cs
+++ Lista 1/Q1.cs
@@ -2,7 +2,6 @@ using System;
   
 class Program {
   public static void Main(string[] args) {
-    //Mostrar uma mensagem na tela com seu nome.
     string x = Console.ReadLine();
     Console.WriteLine($"Seu nome é: {x}");   
   }
