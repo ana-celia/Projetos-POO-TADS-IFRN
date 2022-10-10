6dca9b706a989c1f027b9334d2bc02a0c59f2db7
ana-celia <112821127+ana-celia@users.noreply.github.com>
2022-10-10 09:50:36 -0300

    Create Q1.cs

---
+++ Lista 1/Q1.cs
@@ -0,0 +1,9 @@
+using System;
+  
+class Program {
+  public static void Main(string[] args) {
+    //Mostrar uma mensagem na tela com seu nome.
+    int x = Console.ReadLine();
+    Console.WriteLine("Seu nome é:", x);   
+  }
+}
